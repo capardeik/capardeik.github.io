5866a50e07063037e0ff774af102caf6744c7500
capardeik <capardeik@gmail.com>
2025-09-13 12:55:06 -0500

    descriptions and automation
    add desc. to john and abbi, add expand/collapse family button

---
+++ automation.js
@@ -0,0 +1,10 @@
+function ShowFamily() {
+    document.getElementById("familyexpand").style.display="block";
+    document.getElementById("familyexpandbutton").style.display="none";
+    document.getElementById("familycollapsebutton").style.display="block";
+}
+function HideFamily() {
+    document.getElementById("familyexpand").style.display="none";
+    document.getElementById("familyexpandbutton").style.display="block";
+    document.getElementById("familycollapsebutton").style.display="none";
+}
