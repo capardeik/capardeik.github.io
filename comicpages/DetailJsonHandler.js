82ea47e4e42e29ad4873f40e4f6014f7fbaeade1
capardeik <capardeik@gmail.com>
2025-09-13 15:48:56 -0500

    update detail json handler to match current version

--- comicpages/DetailJsonHandler.js
+++ comicpages/DetailJsonHandler.js
@@ -14,15 +14,41 @@ fetch('../comics.json')
     }
 
     container.innerHTML = `
-      <h1>${comic.title}</h1>
-      <img src="${comic.image}" alt="${comic.title}" width="600">
+      <ul class="originallist">
+      <p class="comicnumber">Mr. Lazoo #${comic.numOnPage}</p>
+      <li class="comic"><img class="comicimage" src="${comic.image}" alt="Comic Image"></li>
+          <li class="comicdetail"><ul class="detailbox">
+              <div class="detailinfo">
+              <li class="detail">In Series: ${comic.numInSeries}</li>
+              <li class="detail">Panels: ${comic.panels}</li>
+              <li class="detail">Color: ${comic.color ? "Yes" : "No"}</li>
+              </div>
+              <li class="detailtitle">Title Panel</li>
+              <li class="detail">${comic.titlePanelType}</li>
+              <li class="detailtitle">Panel 1</li>
+              <li class="detail">Description here</li>
+              <li class="detailtitle">Panel 2</li>
+              <li class="detail">Mr. Lazoo in his car drives over a medium ramp.</li>
+              <li class="detailtitle">Panel 3</li>
+              <li class="detail">Mr. Lazoo in his car drives over a large ramp.</li>
+              <li class="detailtitle">Panel 4</li>
+              <li class="detail">Mr. Lazoo in his car drives over a giant ramp.</li>
+
+
+
       <p><strong>Characters:</strong> ${comic.characters.join(', ')}</p>
       <p><strong>Location:</strong> ${comic.location}</p>
       <p><strong>Panels:</strong> ${comic.panels}</p>
       <p><strong>Color:</strong> ${comic.color ? "Yes" : "No"}</p>
       <h3>Panel Descriptions</h3>
       <ol>
-        ${comic.panel_descriptions.map(p => `<li>${p}</li>`).join('')}
+        ${comic.panel_descriptions.map((p, i) => `
+          <li class="detailtitle">Panel ${i + 1}</li>
+          <li class="detail">${p}</li>
+        `).join('')}
       </ol>
-    `;
+              </ul></li>
+          <a class="link" href="../1.html"><p class="back">Back to #${comics.section}</p></a>
+      </ul>
   });
+
